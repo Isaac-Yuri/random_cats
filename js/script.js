6caf287da06bf6b90b2578e80cbd4562f6b30621
Isaac Yuri <isaacyuri22@gmail.com>
2024-04-25 10:31:09 -0300

    :lipstick: feat: atualização no estilo da página

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
 const button_random_cat = document.getElementById("button-random-cat");
 const cats_area = document.getElementById("cats");
+const loading = document.getElementById("loading");
 const api = "https://api.thecatapi.com/v1/images/search";
 
-button_random_cat.addEventListener('click', () => {    
-    fetch(api)
-        .then((response) => {
-            return response.json();
-        })
-        .then((json) => {
-            let image_url = json[0].url;
-            addNewCat(image_url)
-        })
-        .catch(error => console.error(error))
-})
+button_random_cat.addEventListener("click", () => {
+  loading.classList.add("show"); // Mostra o texto de carregamento
+  fetch(api)
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      let image_url = json[0].url;
+      addNewCat(image_url);
+    })
+    .catch((error) => console.error(error))
+    .finally(() => {
+      loading.classList.remove("show"); // Remove o texto de carregamento após o término da requisição
+    });
+});
 
 function addNewCat(url) {
-    cats_area.innerHTML += `<img src="${url}" class="cat-box" />`
+  cats_area.innerHTML = `<img src="${url}" class="cat-image" />`;
 }
